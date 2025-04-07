de652c804e881537c296b194ca25ceb2fa05d991
tatsuya-okubo-123 <f1121060@gmail.com>
2025-04-07 22:02:46 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const clickBtn=document.getElementById('btn');
+const childtext=document.getElementById('text');
+
+clickBtn.addEventListener('click',()=>{
+  childtext.textContent='ボタンをクリックしました'
+
+
+});
